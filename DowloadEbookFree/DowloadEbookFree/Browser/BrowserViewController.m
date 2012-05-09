4e566f00a0e7243f81410a393d5fbfff3a1928ef
cuc_nguyen <vancucit@gmail.com>
2012-05-10 00:32:26 +0700

    test2 kk

--- DowloadEbookFree/DowloadEbookFree/Browser/BrowserViewController.m
+++ DowloadEbookFree/DowloadEbookFree/Browser/BrowserViewController.m
@@ -43,7 +43,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //vai chùa 
+    //oh my god    
     _webView.delegate = self;
    // [self loadURL:[NSURL URLWithString:@"http://www.gutenberg.org/files/2591/2591-pdf.pdf"]];
     //[self loadURL:[NSURL URLWithString:@"https://github.com/vfr/Reader/zipball/master"]];
